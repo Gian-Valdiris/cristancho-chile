5d701fb2815ec4326eb8d4df63a6a314b8a7f6ce
valdirisblue <gian.valdiris@blue.cl>
2025-06-25 10:09:41 -0500

    Version 2 finalizada

---
+++ index.py
@@ -0,0 +1,17 @@
+import suma 
+
+name = str(input("Enter your name : "))
+age = int(input("Enter your age : "))
+
+
+name1 = str(input("Enter your name : "))
+age1 = int(input("Enter your age : "))
+
+print(f"Hello {name}! You are {age} years old.")
+print(f"Hello {name1}! You are {age1} years old.")
+
+
+print(suma.suma(age, age1))
+
+
+
