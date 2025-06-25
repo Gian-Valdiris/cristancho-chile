94b148aa4fb66d6147c37be0cc3b33779db59cdb
valdirisblue <gian.valdiris@blue.cl>
2025-06-25 10:13:53 -0500

    mejoras de la segunda version

--- index.py
+++ index.py
@@ -1,17 +1,19 @@
 import suma 
 
-name = str(input("Enter your name : "))
-age = int(input("Enter your age : "))
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
 
 
-name1 = str(input("Enter your name : "))
-age1 = int(input("Enter your age : "))
+name1 = str(input("Enter your name 1: "))
+age1 = int(input("Enter your age  1: "))
 
 print(f"Hello {name}! You are {age} years old.")
 print(f"Hello {name1}! You are {age1} years old.")
 
 
-print(suma.suma(age, age1))
+
+print("La suma de las edades es: ", suma.suma(age, age1))
+
 
 
 
